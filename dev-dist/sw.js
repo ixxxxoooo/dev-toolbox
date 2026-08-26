b892030318fd0070ede1646b3febdcfe7dc2805a
Jason <393787391@qq.com>
2026-08-27 00:59:19 +0800

    chore: 更新 PWA 缓存版本

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tc13scgcaa8"
+    "revision": "0.pu5qq4o9ru8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
